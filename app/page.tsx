ee3d09998d21ead093488d3168b2dcc6695c419b
momo-41 <minimizu41@gmail.com>
2024-05-10 19:26:04 +0900

    ボタンの色を変更

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Box, Button, Typography } from "@mui/material";
 export default function Home() {
   return (
     <Box>
-      <Button variant="contained" href="/test">
+      <Button variant="contained" color="inherit" href="/test">
         Contained
       </Button>
       <Typography>muiの勉強会</Typography>
