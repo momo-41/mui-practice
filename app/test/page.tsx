defe5f025c0b539ddd24b28c360bb3336664c918
momo-41 <minimizu41@gmail.com>
2024-05-10 19:56:23 +0900

    レスポンシブ対応

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,7 +4,18 @@ import React from "react";
 const Page = () => {
   return (
     <Box>
-      <Typography>Hello World!</Typography>
+      <Typography
+        fontSize={{ xs: 20, sm: 30, md: 40, lg: 50 }}
+        color={"white"}
+        bgcolor={"lightgreen"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        mt={3}
+        p={10}
+      >
+        Hello World!
+      </Typography>
     </Box>
   );
 };
